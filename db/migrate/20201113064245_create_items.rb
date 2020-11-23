2e3003468998c0416b6251dcf8e1c5e43d5efb89
yasu971 <yasuishii1421@gmail.com>
2020-11-23 13:57:49 +0900

    rails db migrate
    ユーザーズテーブルから、先にアップにする必要がある。

--- db/migrate/20201113064245_create_items.rb
+++ db/migrate/20201113064245_create_items.rb
@@ -1,6 +1,16 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
+      t.string :name,                     null: false
+      t.text :description,                null: false
+      t.integer :category_id,             null: false
+      t.integer :item_condition_id,       null: false
+      t.integer :delivery_fee_burden_id,  null: false
+      t.integer :prefectures_id,          null: false
+      t.integer :delivery_period_id,      null: false
+      t.integer :price,                   null: false
+      # t.references :user, foreign_key: true
+      t.references :user, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -13,8 +13,18 @@
 ActiveRecord::Schema.define(version: 2020_11_18_075811) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.integer "category_id", null: false
+    t.integer "item_condition_id", null: false
+    t.integer "delivery_fee_burden_id", null: false
+    t.integer "prefectures_id", null: false
+    t.integer "delivery_period_id", null: false
+    t.integer "price", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -35,4 +45,5 @@ ActiveRecord::Schema.define(version: 2020_11_18_075811) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
