2b6ada1bcd3e1f885c5c8b6c8286be9fb5628b10
yasu971 <yasuishii1421@gmail.com>
2020-11-23 17:25:16 +0900

    itemモデル編集
      extend ActiveHash::Associations::ActiveRecordExtensions
    バリデーション設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :choice
+  validates :name, :description, :category_id, :item_condition_id, :delivery_fee_burden_id, :delivery_fee_burden_id, :delivery_period_id, :price, :image, presence: true
+  
 end
