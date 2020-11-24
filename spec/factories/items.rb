7734bc9e1bdce0452c766cfc2a76cf0da6b5cfed
yasu971 <yasuishii1421@gmail.com>
2020-11-24 16:54:09 +0900

    itemに関するFactoryBot編集

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name                    {"商品名"}
+    description             {Faker::Lorem.sentence}
+    category_id             {1}
+    item_condition_id       {2}
+    delivery_fee_burden_id  {3}
+    prefectures_id          {18}
+    delivery_period_id      {4}
+    price                   {3000}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
