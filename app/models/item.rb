c295b0f01fdcfc89baa10ce6ee7fc9d97e439c54
yasu971 <yasuishii1421@gmail.com>
2020-11-24 13:33:12 +0900

    Itemモデル編集、choiceの記述削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,7 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to :choice, :prefecture, :category, :delivery_fee_burden, :delivery_period, :item_condition
+  belongs_to :prefecture, :category, :delivery_fee_burden, :delivery_period, :item_condition
   validates :name, :description, :category_id, :item_condition_id, :delivery_fee_burden_id, :delivery_fee_burden_id, :delivery_period_id, :price, :image, presence: true
   
 end
