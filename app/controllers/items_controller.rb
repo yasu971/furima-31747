34ac01bd451dd8274a8325773296ef271350645e
yasu971 <yasuishii1421@gmail.com>
2020-11-24 12:56:38 +0900

    Update items_controller.rb
    newアクション
    createアクション

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,5 +5,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :item_condition_id, :delivery_fee_burden_id, :prefectures_id, :delivery_fee_burden_id, :delivery_period_id, :price, :image).merge(user_id: current_user.id)
+  end  
+
 end
