dd24ee8667d8add7f750aa84df628b677603f487
yasu971 <yasuishii1421@gmail.com>
2020-11-25 12:34:36 +0900

    JavaScriptファイル編集

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,3 +1,14 @@
-window.addEventListener('load', () => {
-  console.log("OK");
-});
+window.addEventListener('load', function(){
+
+  const priceInput = document.getElementById("item-price")
+  const addTaxDom = document.getElementById("add-tax-price")
+  const Profit = document.getElementById("profit")
+
+  priceInput.addEventListener('input', function(){
+    const inputValue = priceInput.value
+    console.log(inputValue)
+    addTaxDom.innerHTML = Math.floor(inputValue / 10)
+    Profit.innerHTML = Math.floor(inputValue * 0.9)
+  })
+})
+
