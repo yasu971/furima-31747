dab1455efded03f82748cbf92e94142580336e19
yasu971 <yasuishii1421@gmail.com>
2020-11-30 07:24:13 +0900

    items_controller編集
    以下のアクション編集
    edit,update

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,23 +23,23 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-  #   if @item.user == current_user
-  #     render "edit"
-  #   else
-  #     redirect_to root_path
-  #   end
-  # end
+  def edit
+    @item = Item.find(params[:id])
+    if @item.user == current_user
+      render "edit"
+    else
+      redirect_to root_path
+    end
+  end
 
-  # def update
-  #   @item = Item.find(params[:id])
-  #   if @item.update(item_params)
-  #     redirect_to item_path
-  #   else
-  #     render :edit
-  #   end
-  # end
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path
+    else
+      render :edit
+    end
+  end
 
   private
 
