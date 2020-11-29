58b96c2d13fa0d9ba15bcaf92c96e4fb87d9c870
yasu971 <yasuishii1421@gmail.com>
2020-11-29 21:05:05 +0900

    コメントアウト削除等
    修正前:prefectures_id
    修正後:prefecture_id

--- db/migrate/20201118075812_create_items.rb
+++ db/migrate/20201118075812_create_items.rb
@@ -6,11 +6,9 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer :category_id,             null: false
       t.integer :item_condition_id,       null: false
       t.integer :delivery_fee_burden_id,  null: false
-      # t.integer :prefectures_id,          null: false
       t.integer :prefecture_id,          null: false
       t.integer :delivery_period_id,      null: false
       t.integer :price,                   null: false
-      # t.references :user, foreign_key: true
       t.references :user, foreign_key: true
       t.timestamps
     end
