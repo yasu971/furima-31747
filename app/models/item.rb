af374b507d330fe1312f628307780d31cdbac1d4
yasu971 <yasuishii1421@gmail.com>
2020-11-30 15:19:43 +0900

    itemモデル編集2
    スペース削除

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,5 +10,5 @@ class Item < ApplicationRecord
   validates :name, :description ,:image, presence: true
   validates :category_id, :item_condition_id, :delivery_fee_burden_id, :prefecture_id, :delivery_period_id, numericality: { other_than: 1 } 
   
-  validates :price, presence: true,  numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000, message: 'Price is out of range' }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000, message: 'Price is out of range' }
 end
