d360a97b4dec71e957b3a852b57bd9f574a8be0c
yasu971 <yasuishii1421@gmail.com>
2020-12-01 12:07:25 +0900

    バリデーション
    1行ずつに。

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,7 +7,9 @@ class Item < ApplicationRecord
   belongs_to :delivery_fee_burden
   belongs_to :prefecture
   belongs_to :delivery_period
-  validates :name, :description ,:image, presence: true
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :image, presence: true
   validates :category_id, :item_condition_id, :delivery_fee_burden_id, :prefecture_id, :delivery_period_id, numericality: { other_than: 1 } 
   
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than: 10_000_000, message: 'Price is out of range' }
