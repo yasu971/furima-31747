aef52f01b99d15c48bab5b10e9f68b3a961f10ab
yasu971 <yasuishii1421@gmail.com>
2020-12-02 11:13:39 +0900

    rails db:migrate:create_addresses
    マイグレーション適用:create_addresses

--- db/migrate/20201201124757_create_addresses.rb
+++ db/migrate/20201201124757_create_addresses.rb
@@ -1,6 +1,14 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
+      t.references :order,              foreign_key: true
+      t.string :postal_code,            null: false
+      t.text :description,              null: false
+      t.integer :prefecture_id,         null: false
+      t.string :municipality,           null: false
+      t.string :house_number
+      t.string :building_name,          null: false
+      t.string :telephone_number,       null: false
 
       t.timestamps
     end
