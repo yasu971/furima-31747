a659c1a40798ab366d25d184dde62e7be1bd2276
yasu971 <yasuishii1421@gmail.com>
2020-12-02 10:21:14 +0900

    ルーティング設定(ネスト)修正

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
-  resources :orders, only: [:index, :create] do
-    resources :addresses, only: :create
+  resources :items do
+    resources :orders, only: [:index, :create]
   end  
+  # resources :orders, only: [:index, :create] do
+  #   resources :addresses, only: :create
+  # end  
 end
