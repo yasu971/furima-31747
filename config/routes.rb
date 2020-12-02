acb78ec4516c45d0f2624c8b560ae1d2663b9cdf
yasu971 <yasuishii1421@gmail.com>
2020-12-02 09:55:59 +0900

    ルーティング設定(ネスト)
    親:orders
    子:addresses

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "items#index"
   resources :items
+  resources :orders, only: [:index, :create] do
+    resources :addresses, only: :create
+  end  
 end
