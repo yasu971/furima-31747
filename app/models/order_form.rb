646fe357c8af4ebfdfa6b969ec3a393d7c790ec0
yasu971 <yasuishii1421@gmail.com>
2020-12-03 17:37:03 +0900

    Update order_form.rb
    バリデーション
    saveメソッド編集

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -1,16 +1,19 @@
 class OrderForm
   include ActiveModel::Model
-  attr_accessor :order, :user, :item, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number
+  attr_accessor :order_id, :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number
 
-  validates :order, presence: true
-  validates :postal_code, presence: true
-  validates :prefecture_id, presence: true
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :telephone_number, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
+  end  
+  validates :prefecture_id, numericality: { other_than: 1 } 
   validates :municipality, presence: true
   validates :house_number, presence: true
   validates :telephone_number, presence: true
   
   def save
-    Order.create(order: order)
-    Address.create(address: address)
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(order_id: order.id, postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, house_number: house_number, building_name: building_name, telephone_number: telephone_number)
   end
 end
+
