625c70beb5ba45c3f6f6710659e0c945c1934183
yasu971 <yasuishii1421@gmail.com>
2020-12-03 17:37:12 +0900

    Update orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,31 @@
 class OrdersController < ApplicationController
 
   def index
-    @order = OrderForm.new
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new
   end
-  
+
+  def new
+    # @order = OrderForm.new
+    # @order = Order.new
+    @order_form = OrderForm.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_form = OrderForm.new(set_params)
+    if @order_form.valid?
+      @order_form.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render :index
+    end
+  end
+ 
+  private
+  def set_params
+    params.require(:order_form).permit(:user, :item, :order, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
 end
