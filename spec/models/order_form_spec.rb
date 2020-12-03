2dee884e21a0bc0e8abcf7c74085b55e575c5ea4
yasu971 <yasuishii1421@gmail.com>
2020-12-04 08:55:30 +0900

    Update order_form_spec.rb
    一旦、コミット
    ※12/3(木)作業分

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe OrderForm, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '住所情報の保存' do
+    before do
+      @order_form = FactoryBot.build(:order_form)
+    end
+
+    it 'すべての値が正しく入力されていれば保存できること' do
+
+    end
+    it '郵便番号が空だと保存できないこと' do
+
+    end
+    it '市区町村が空だと保存できないこと' do
+
+    end
+    it '番地が空だと保存できないこと' do
+    end
+    
+
+
+  end
 end
