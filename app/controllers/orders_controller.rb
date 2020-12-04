c9589946e662d61e6b8091b133f0ad1b909865fe
yasu971 <yasuishii1421@gmail.com>
2020-12-04 19:13:35 +0900

    orders_controller編集
    クレジットカード決済機能実装のため

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -14,7 +14,9 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(set_params)
+    # binding.pry
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -26,6 +28,15 @@ class OrdersController < ApplicationController
   private
   def set_params
     params.require(:order_form).permit(:user, :item, :order, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: set_params[:token],
+      currency: 'jpy'
+    )
   end
 
 end
