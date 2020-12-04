182b8e15f784e3c2d22b91d09b301e023885f3ab
yasu971 <yasuishii1421@gmail.com>
2020-12-04 22:30:24 +0900

    ファクトリーボット編集
    +prefecture_id { 2 }

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,10 +1,13 @@
 FactoryBot.define do
   factory :order_form do
     postal_code { '123-4567' }
-    prefecture { 2 }
+    prefecture_id { 2 }
     municipality { '札幌市' }
     house_number { '青山1-1' }
     building_name { '東京ハイツ' }
     telephone_number { '09012345678' }
+    
+    # @item.price {3000}
+    token {"tok_abcdefghijk00000000000000000"}
   end
 end
