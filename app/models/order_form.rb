bfa4ecfa8561ec8299a2656e6ba6f192cff95fbc
yasu971 <yasuishii1421@gmail.com>
2020-12-06 15:44:06 +0900

    Update order_form.rb
    user_id,item_id
    のバリデーション追記

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,10 +2,13 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :order_id, :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number, :token
 
+  validates :user_id, presence: true
+  validates :item_id, presence: true
+
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
     validates :telephone_number, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
-  end  
+  end
   validates :prefecture_id, numericality: { other_than: 1 } 
   validates :municipality, presence: true
   validates :house_number, presence: true
