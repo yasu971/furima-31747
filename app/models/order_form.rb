834379322bb902dc17cf15fd09616e45e4828fb6
yasu971 <yasuishii1421@gmail.com>
2020-12-06 19:13:07 +0900

    Update order_form.rb
    リファクタリング
    presence: trueの複数、記述をまとめる。

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,18 +2,18 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :order_id, :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number, :token
 
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-
   with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :token
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :municipality
+    validates :house_number
+    validates :telephone_number
     validates :telephone_number, format: {with: /\A[0-9]+\z/, message: "is invalid. Input half-width characters."}
   end
+  
   validates :prefecture_id, numericality: { other_than: 1 } 
-  validates :municipality, presence: true
-  validates :house_number, presence: true
-  validates :telephone_number, presence: true
-  validates :token, presence: true
   
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
