7b4d39a711c5006075ede50f9210be9aed0dbcd9
yasu971 <yasuishii1421@gmail.com>
2020-12-07 19:34:56 +0900

    Update order_forms.rb
    別ファイルにて、userとitemを呼び出す。

--- spec/factories/order_forms.rb
+++ spec/factories/order_forms.rb
@@ -1,14 +1,12 @@
 FactoryBot.define do
   factory :order_form do
-    user_id { 1 }
-    item_id { 2 }
     postal_code { '123-4567' }
     prefecture_id { 2 }
     municipality { '札幌市' }
     house_number { '青山1-1' }
     building_name { '東京ハイツ' }
     telephone_number { '09012345678' }
-    
     token {"tok_abcdefghijk00000000000000000"}
+
   end
 end
