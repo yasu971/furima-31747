de19fe49c40903c814b41c78b5ca06450f2793d2
yasu971 <yasuishii1421@gmail.com>
2021-11-03 23:18:28 +0900

    Update category.rb

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,18 +1,17 @@
 class Category < ActiveHash::Base
   self.data = [
     { id: 1, name: '--' },
-    { id: 2, name: 'レディース' },
-    { id: 3, name: 'メンズ' },
-    { id: 4, name: 'ベビー・キッズ' },
-    { id: 5, name: 'インテリア・住まい・小物' },
-    { id: 6, name: '本・音楽・ゲーム' },
-    { id: 7, name: 'おもちゃ・ホビー・グッズ' },
-    { id: 8, name: '家電・スマホ・カメラ' },
-    { id: 9, name: 'スポーツ・レジャー' },
-    { id: 10, name: 'ハンドメイド' },
-    { id: 11, name: 'その他' }
+    { id: 2, name: '制服' },
+    { id: 3, name: '書籍' },
+    { id: 4, name: '辞書' },
+    { id: 5, name: '家電' },
+    { id: 6, name: '家具' },
+    { id: 7, name: '文具' },
+    { id: 8, name: '服' },
+    { id: 9, name: 'その他生活雑貨' }
   ]
 
+  
   include ActiveHash::Associations
   has_many :items
 
